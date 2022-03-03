dfcc6bb6691e11cf002ef3f5f21a8264f78f848d
AnoopAmbunhi <anoop.ambunhi@seera.sa>
2022-03-03 10:58:19 +0400

    Cypress Test Assignment Session 3

---
+++ cypress/integration/test/FlightSearchTest.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress"/>
+import FlightListingPO from "../pageObjects/FlightListingPO";
+
+describe('Flight Search', function () {
+    it('Flight Search with Direct Stop and Saudia Airlines', function () {
+        const flightListingPO = new FlightListingPO()
+        cy.visit("https://next-staging-ae.almosafer.com/en/flights/DXB-JED/2022-05-22/2022-05-25/Economy/1Adult")
+        flightListingPO.checkDirectFlights()
+        flightListingPO.showAllFilters();
+        flightListingPO.checkSaudiaFlightsFilter()
+        flightListingPO.selectFirstFlight()
+    })
+})
